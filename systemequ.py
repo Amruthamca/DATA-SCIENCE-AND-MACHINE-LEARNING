f79cf9bc44d3dc66521ac9b54cc8969369fba87a
Amruthamca <94528537+Amruthamca@users.noreply.github.com>
2023-01-04 18:21:04 +0530

    Add files via upload

---
+++ systemequ.py
@@ -0,0 +1,13 @@
+print("Amrutha Biju")
+print("21mca003")
+import numpy as np
+A = np.array([[2, 1, -2],
+              [3, 0, 1],
+              [1, 1, -1]])
+b = np.array([[-3],
+              [5],
+              [-2]])
+a=np.linalg.inv(A)
+x= np.linalg.solve(a, b)
+print("Value of X=A -1 b: ")
+print(x)
